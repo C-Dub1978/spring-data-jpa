05534b9ae92efbb45a6f6c4eaf8f4c050aec9fb4
TransientCW <chris@myubercode.com>
2021-11-17 21:46:49 -0700

    init repo

---
+++ database-demo/src/test/java/site/cswilson/database/databasedemo/DatabaseDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package site.cswilson.database.databasedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
